24f5b2b092d2177e922ab07c008cb8188d0f9d68
enricofrigo <enrico.frigo@gmail.com>
2025-08-06 15:40:48 +0200

    v0.0.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
         minSdk = 30
         targetSdk = 36
         versionCode = 1
-        versionName = "0.0.2"
+        versionName = "0.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
