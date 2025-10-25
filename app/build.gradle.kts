a52f55970b593c4cadc78b2977281fa869f522f4
enricofrigo <enrico.frigo@gmail.com>
2025-10-25 12:08:24 +0200

    v0.0.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
         applicationId = "eu.frigo.dispensa"
         minSdk = 30
         targetSdk = 36
-        versionCode = 1
+        versionCode = 6
         versionName = "0.0.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
