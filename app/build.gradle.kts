4313218110fe23695fd5e78323d84c538564c279
enricofrigo <enrico.frigo@gmail.com>
2025-11-15 11:45:36 +0100

    unsigned

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,7 +23,6 @@ android {
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
-            signingConfig = signingConfigs.getByName("debug")
         }
     }
     compileOptions {
