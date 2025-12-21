25fe94065e4ee6b1182ef4e0179dfa200f280eed
enricofrigo <enrico.frigo@gmail.com>
2025-12-21 16:12:06 +0100

    version 8 first play store

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "eu.frigo.dispensa"
         minSdk = 26
         targetSdk = 35
-        versionCode = 7
-        versionName = "0.0.7"
+        versionCode = 8
+        versionName = "0.0.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("String", "GITHUB_TOKEN", project.properties["GITHUB_API_TOKEN"].toString())
