0bd33ad759bc1bf76eec5502140a2f394d8685f8
enricofrigo <enrico.frigo@gmail.com>
2026-03-16 22:41:36 +0100

    v 0.1.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 android {
     namespace = "eu.frigo.dispensa"
-    compileSdk = 35
+    compileSdk = 36
     buildFeatures{
         buildConfig = true
     }
@@ -14,8 +14,8 @@ android {
         applicationId = "eu.frigo.dispensa"
         minSdk = 26
         targetSdk = 35
-        versionCode = 14
-        versionName = "0.1.4"
+        versionCode = 15
+        versionName = "0.1.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
